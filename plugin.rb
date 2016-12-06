8dd710e72a64f15507dae85e5d5a397370c8618b
Daniel Marquard <daniel@netidy.com>
2016-12-06 16:15:08 -0500

    Updated to 1.0

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Overwatch Hero Icons
 # about: Formats Overwatch hero names in posts, prepending hero icons and correcting hero spelling and character case.
-# version: 0.1 alpha
+# version: 1.0
 # authors: Daniel Marquard
 # url: https://github.com/critcola/discourse-overwatch-hero-icons
 
